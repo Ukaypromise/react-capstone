84ac0d89738bdb667607ac718cffca39f8168b97
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-10-05 22:19:32 +0100

    create routes and navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import {
+  BrowserRouter as Router, Routes, Route, Switch,
+} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import ContentInfo from './components/ContentInfo';
+// Styles
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Crypto App</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <GlobalStyles />
+        <div>
+          <Navbar />
+        </div>
+        <section className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Content" element={<ContentInfo />} />
+          </Routes>
+        </section>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+      <Link className="navLink" to="/">
+        <h1 className="nameNav">Crypto</h1>
+      </Link>
+    </div>
+  );
+}
+
+export default Navbar
